acca3606c988f658f44cefe3731073e90310cfa7
lorenaraquelolive <lorenaraquelolive@academico.ufs.br>
2024-11-02 12:18:50 -0300

    Modal

--- app.js
+++ app.js
@@ -36,3 +36,24 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+
+document.addEventListener("DOMContentLoaded", function() {
+    const modal = document.getElementById("modal"); 
+    const btn = document.getElementById("trocarServicoBtn");
+    const span = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
+
